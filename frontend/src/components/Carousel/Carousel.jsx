bce6ac56ce1a6e05f5fb4b49e78753c62ded3d9d
vinimateuslima <viniciusmateus.dev@gmail.com>
2024-08-05 20:07:13 -0300

    em andamento

---
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,51 @@
+import "./Carousel.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const Carousel = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+        <div>
+          <h3>9</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,13 +1,45 @@
 import Header from "../components/Header";
+import Carousel from "../components/Carousel/Carousel";
+import axios from "../axios-config";
 
-import './Home.css'
+import { useState, useEffect } from "react";
+
+import "./Home.css";
 
 const Home = () => {
+ // const [books, setBooks] = useState([]);
+  const [categories, setCategories] = useState({});
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const res = await axios.get("users/books/66a3f31891ae8751357cda7f");
+
+      const categorizedBooks = res.data.reduce((acc, book) => {
+        book.bookId.categories.forEach((category) => {
+          if (!acc[category]) {
+            acc[category] = [];
+          }
+          acc[category].push(book);
+        });
+        return acc;
+      }, {});
+
+      setCategories(categorizedBooks);
+    };
+
+    getBooks();
+  }, []);
+
   return (
     <>
       <Header />
       <div id="home">
-        Home
+        <h1 className="bem-vindo">
+          Bem vindo(a), <span>Usuário</span>
+        </h1>
+        <Carousel />
+
+        {console.log(categories)}
       </div>
     </>
   );
