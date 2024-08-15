4d7520994d836dc3a9401139eaaefa1f0ed72c96
vinimateuslima <viniciusmateus.dev@gmail.com>
2024-08-15 00:35:56 -0300

    atualizando pagina de reviewbook

--- frontend/src/components/ReviewBook.jsx
+++ frontend/src/components/ReviewBook.jsx
@@ -1,27 +1,66 @@
-import './ReviewBook.css'
+import { useState } from "react";
+import "./ReviewBook.css";
 
 import * as FaIcons from "react-icons/fa6";
 
 const ReviewBook = ({ data, updateFieldHandler }) => {
+  const [rating, setRating] = useState(null)
+  const [hover, setHover] = useState(null)
+  const [page, setPage] = useState("0");
+console.log(rating)
+  const handleCurrentPage = (e) => {
+    const valorAtual = e.target.value;
+
+    if (valorAtual >= 0 && valorAtual <= data.pageCount) {
+      updateFieldHandler({ currentPage: e.target.value });
+      console.log(valorAtual);
+    } else {
+      console.log("valor não pode ser maior que o número de páginas");
+    }
+  };
+
   return (
     <div className="reviewBook">
       <div className="pagina">
-      <h3>Em qual página você está?</h3>
-      <p>(Este livro possui {data.pageCount} páginas)</p>
-      <input type="number" name="" id="" />
+        <h3>Em qual página você está?</h3>
+        <p>(Este livro possui {data.pageCount} páginas)</p>
+        <input
+          type="number"
+          name=""
+          id=""
+          value={data.currentPage || ""}
+          onChange={handleCurrentPage}
+          maxLength={data.pageCount.toString.length}
+          min="0"
+        />
       </div>
       <div className="avaliacao">
         <h3>Como você avalia o livro?</h3>
-        <div className="stars">
-        <FaIcons.FaStar />
-        <FaIcons.FaStar />
-        <FaIcons.FaStar />
-        <FaIcons.FaStar />
-        <FaIcons.FaStar />
-        </div>
+        <div className="stars">{[...Array(5)].map((star, index) => {
+          const currentRating = index + 1;
+          return (
+            <label key={index}>
+              <input type="radio" 
+              name="rating"
+              value={currentRating}
+              onClick={() => setRating(currentRating)}
+              />
+              <FaIcons.FaStar
+              className="star"
+              color={currentRating <= (hover || rating) ? "#ff607f" : "#909090"}
+              onMouseEnter={() => setHover(currentRating)}
+              onMouseLeave={() => setHover(null)}
+              />
+            </label>
+          )
+          
+
+        })}</div>
       </div>
       <div className="resenha">
-        <h3>Escreva uma resenha <span>(opcional)</span></h3>
+        <h3>
+          Escreva uma resenha <span>(opcional)</span>
+        </h3>
         <textarea name="" id=""></textarea>
       </div>
     </div>
