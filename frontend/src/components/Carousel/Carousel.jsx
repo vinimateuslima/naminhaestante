348ae07b1a59c9114686ff125549e9a528475a69
vinimateuslima <viniciusmateus.dev@gmail.com>
2024-08-20 23:07:35 -0300

    atualizando o carousel

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -13,8 +13,6 @@ const Carousel = ({ children }) => {
     swipeToSlide: true,
     slidesToShow: 2,
     arrows: false,
-    centerMode: true,
-    centerPadding: "0px",
     afterChange: function (index) {
       console.log(
         `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
